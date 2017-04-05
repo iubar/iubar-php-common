41f974a2442037d9ca9189103264a28bb3f08124
daniele <daniele.montesi@iubar.it>
2017-04-05 12:36:35 +0200

    Bug fix

--- src/Iubar/Net/EmailValidator.php
+++ src/Iubar/Net/EmailValidator.php
@@ -24,70 +24,73 @@ class EmailValidator {
     	$is_valid = false;
 
     	$mailparts=explode("@",$email);
-    	$hostname = $mailparts[1];
+    	if (isset($mailparts[1])){
+    		$hostname = $mailparts[1];
 
-    	// validate email address syntax
-    	$exp = "/^[a-z\'0-9]+([._-][a-z\'0-9]+)*@([a-z0-9]+([._-][a-z0-9]+))+$/i";
+    		// validate email address syntax
+    		$exp = "/^[a-z\'0-9]+([._-][a-z\'0-9]+)*@([a-z0-9]+([._-][a-z0-9]+))+$/i";
 
-    	$b_valid_syntax=preg_match($exp, $email);
+    		$b_valid_syntax=preg_match($exp, $email);
 
-    	// get mx addresses by getmxrr
-    	$b_mx_avail = true;
-    	if($check_mx){
-    		$b_mx_avail = getmxrr( $hostname, $mx_records, $mx_weight );
-    	}
-    	$b_server_found=0;
-
-    	if($b_valid_syntax && $b_mx_avail){
-
-    		if(!$check_mailbox){
-    			$is_valid = true;
-    		}else{
-
-    		// copy mx records and weight into array $mxs
-    		$mxs=array();
-
-    		for($i=0;$i<count($mx_records);$i++){
-    			$mxs[$mx_weight[$i]]=$mx_records[$i];
+    		// get mx addresses by getmxrr
+    		$b_mx_avail = true;
+    		if($check_mx){
+    			$b_mx_avail = getmxrr( $hostname, $mx_records, $mx_weight );
     		}
+    		$b_server_found=0;
 
-    		// sort array mxs to get servers with highest prio
-    		ksort ($mxs, SORT_NUMERIC );
-    		reset ($mxs);
+    		if($b_valid_syntax && $b_mx_avail){
 
-    		while (list ($mx_weight, $mx_host) = each ($mxs) ) {
-    			if($b_server_found == 0){
+    			if(!$check_mailbox){
+    				$is_valid = true;
+    			}else{
 
-    				//try connection on port 25
-    				$fp = @fsockopen($mx_host,25, $errno, $errstr, 2);
-    				if($fp){
-    					$ms_resp="";
-    					// say HELO to mailserver
-    					$ms_resp.= $this->send_command($fp, "HELO microsoft.com");
+    				// copy mx records and weight into array $mxs
+    				$mxs=array();
 
-    					// initialize sending mail
-    					$ms_resp.= $this->send_command($fp, "MAIL FROM:<support@microsoft.com>");
-
-    					// try receipent address, will return 250 when ok..
-    					$rcpt_text= $this->send_command($fp, "RCPT TO:<".$email.">");
-    					$ms_resp.=$rcpt_text;
-
-    					if(substr( $rcpt_text, 0, 3) == "250"){
-    						$b_server_found=1;
-    						$is_valid = true;
-    					}
-
-    					// quit mail server connection
-    					$ms_resp.= $this->send_command($fp, "QUIT");
-
-    					fclose($fp);
-
+    				for($i=0;$i<count($mx_records);$i++){
+    					$mxs[$mx_weight[$i]]=$mx_records[$i];
     				}
 
+    				// sort array mxs to get servers with highest prio
+    				ksort ($mxs, SORT_NUMERIC );
+    				reset ($mxs);
+
+    				while (list ($mx_weight, $mx_host) = each ($mxs) ) {
+    					if($b_server_found == 0){
+
+    						//try connection on port 25
+    						$fp = @fsockopen($mx_host,25, $errno, $errstr, 2);
+    						if($fp){
+    							$ms_resp="";
+    							// say HELO to mailserver
+    							$ms_resp.= $this->send_command($fp, "HELO microsoft.com");
+
+    							// initialize sending mail
+    							$ms_resp.= $this->send_command($fp, "MAIL FROM:<support@microsoft.com>");
+
+    							// try receipent address, will return 250 when ok..
+    							$rcpt_text= $this->send_command($fp, "RCPT TO:<".$email.">");
+    							$ms_resp.=$rcpt_text;
+
+    							if(substr( $rcpt_text, 0, 3) == "250"){
+    								$b_server_found=1;
+    								$is_valid = true;
+    							}
+
+    							// quit mail server connection
+    							$ms_resp.= $this->send_command($fp, "QUIT");
+
+    							fclose($fp);
+
+    						}
+
+    					}
+    				}
     			}
     		}
     	}
-    	}
+
     	return $is_valid;
     }
 
