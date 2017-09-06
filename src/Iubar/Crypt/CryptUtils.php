91eee70f7a4ad1534bce4f5ee62ef1e70bef4e1e
daniele <daniele.montesi@iubar.it>
2017-09-06 10:32:54 +0200

    Edit CryptUtils.php

--- src/Iubar/Crypt/CryptUtils.php
+++ src/Iubar/Crypt/CryptUtils.php
@@ -4,11 +4,11 @@ namespace Iubar\Crypt;
 
 class CryptUtils {
 
-	function base64UrlEncode($data) {
+	public static function base64UrlEncode($data) {
 		return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
 	}
 
-	function base64UrlDecode($data) {
+	public static function base64UrlDecode($data) {
 		return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
 	}
 
