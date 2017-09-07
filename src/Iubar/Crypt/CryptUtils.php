b53f0ccfdb334c6561fd9e994c3483ab006f80b6
daniele <daniele.montesi@iubar.it>
2017-09-07 11:05:38 +0200

    Edit

--- src/Iubar/Crypt/CryptUtils.php
+++ src/Iubar/Crypt/CryptUtils.php
@@ -4,12 +4,19 @@ namespace Iubar\Crypt;
 
 class CryptUtils {
 
+	private static $PADDING = true; // Se true, qualora $data non contenga i caratteri di padding, li aggiungo
+
 	public static function base64UrlEncode($data) {
-		return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+		return strtr(base64_encode($data), '+/', '-_');
 	}
 
 	public static function base64UrlDecode($data) {
-		return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+		if(self::$PADDING){
+			return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) + (4 - strlen($data) % 4) % 4, '=', STR_PAD_RIGHT));
+		}else{
+			return base64_decode(strtr($data, '-_', '+/'));
+		}
 	}
+
 
 }
