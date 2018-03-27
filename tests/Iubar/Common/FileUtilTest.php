63d4d97395600fe854cac470098bf714d3316c22
borgogelli@iubar.it <borgogelli@iubar.it>
2018-03-27 08:56:03 +0200

    .

--- tests/Iubar/Common/FileUtilTest.php
+++ tests/Iubar/Common/FileUtilTest.php
@@ -1,33 +1,24 @@
 <?php
 
-namespace Iubar\Tests\Common;
+declare(strict_types=1);
+
+namespace Iubar\Tests\Crypt;
 
 use PHPUnit\Framework\TestCase;
 use Iubar\Common\FileUtil;
 
-$target_path = "C:/Users/Borgo/workspace_php/php/php_iubar_builder/resources/pagheopen/jar";
-$files1 = FileUtil::getFilesInPath($target_path, $ext = "", false);
-$files2 = FileUtil::getFilesInPath($target_path, $ext = "", true);
- 
+class FileUtilTest extends TestCase {
 
-echo "Files: " . count($files1) . PHP_EOL;
-echo "Files: " . count($files2) . PHP_EOL;
- 
+	const REGEX_SQL = "/update_db_(.*)\\.sql/i";
 
-echo "==================================" . PHP_EOL;
+	public function testGetFileByPattern1(){
+		$dir = __DIR__ . DIRECTORY_SEPARATOR . 'emtpy-dir';
+		$files = FileUtil::getFileByPattern($dir, self::REGEX_SQL);
+	}
 
-foreach ($files1 as $filesinfo) {
-	echo "file " . $filesinfo->getBasename() . PHP_EOL;
+	public function testGetFileByPattern2(){
+		$dir = __DIR__ . DIRECTORY_SEPARATOR . 'locked-dir';
+		$files = FileUtil::getFileByPattern($dir, self::REGEX_SQL);
+	}
+
 }
-
-echo "==================================" . PHP_EOL;
-
-foreach ($files2 as $filesinfo) {
-	echo "file " . $filesinfo->getBasename() . PHP_EOL;
-}
-
-echo "==================================" . PHP_EOL;
-
-echo "END" . PHP_EOL;
-
- 
