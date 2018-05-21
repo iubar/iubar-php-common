d9d9316b47c4aa6acd14ac7095892d770a331b33
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2018-05-21 19:44:56 +0200

    no message

--- src/Iubar/Net/TelegramNotifier.php
+++ src/Iubar/Net/TelegramNotifier.php
@@ -5,18 +5,16 @@ namespace Iubar\Net;
 
 class TelegramNotifier {
 
-	public static function notify($message) {
-		$config_file = realpath(__DIR__) . '/../../../../config/build.properties';
+	public static function notify($title, $message) {
+		$config_file = realpath(__DIR__) . '/../../../config/config.ini';
 		if (!is_file($config_file)) {
-			die("Missing $config_file" . PHP_EOL);
+			die("The config file is missing: $config_file" . PHP_EOL);
 		}
-		$config = parse_ini_file($config_file);
-		
+		$config = parse_ini_file($config_file);		
 		$token = $config['telegram.token'];
 		$chat_id = $config['telegram.chatid'];
-		$app_name = "IUBAR BUILDER";
 		
-		$message = '<b>' . $app_name . '</b>' . PHP_EOL . $message;
+		$message = '<b>' . $title . '</b>' . PHP_EOL . $message;
 		$get_req = '/bot' . $token . '/sendMessage?chat_id=' . $chat_id . '&parse_mode=HTML&text=' . urlencode($message);
 		$response = file_get_contents($get_req);
 		// oppure
