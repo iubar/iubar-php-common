d1dc77394361978bf83c1394cb5dac2945bc44dd
daniele <daniele.montesi@gmail.com>
2019-08-30 11:12:33 +0200

    .

--- tests/Iubar/Common/FileUtilTest.php
+++ tests/Iubar/Common/FileUtilTest.php
@@ -7,10 +7,14 @@ namespace Iubar\Tests\Crypt;
 use Iubar\Common\FileUtil;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @TODO : su GitHub non ci sono le cartelle 'empty-dir' e 'locked-dir' e sul file .gitignore vengono incluse, perchè non ci sono?
+ * Dove vengono create?
+ * Test commentato in phpunit.xml
+ */
 class FileUtilTest extends TestCase {
 
 	const REGEX_SQL = "/update_db_(.*)\.sql/i";
-
 	public function testGetFileByPattern1(){
 		$dir = __DIR__ . DIRECTORY_SEPARATOR . 'emtpy-dir';
 		$files = FileUtil::getFileByPattern($dir, self::REGEX_SQL);
