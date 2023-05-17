eb3a4a3bc04a765888b822db0437128e376c2ad6
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2023-05-17 09:26:22 +0200

    .

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-error_reporting(E_ALL | E_STRICT);
-
-require realpath(__DIR__) . '/../vendor/autoload.php';
-//
-// in alternativa
-// $autoloader = require __DIR__ . '/../vendor/autoload.php';
-// $autoloader->addPsr4('Iubar\\Tests\\', __DIR__);
