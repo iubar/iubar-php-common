1ca42de7658df1027651e70b2696532f3ed41102
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2023-11-21 10:51:33 +0100

    .

--- src/Iubar/Crypt/AesBase.php
+++ src/Iubar/Crypt/AesBase.php
@@ -3,30 +3,30 @@
 namespace Iubar\Crypt;
 
 abstract class AesBase {
-	public $key = null;
+    public string $key = '';
 
 	private $sig_delimiter = ':';
 
-	abstract public function encrypt($plaintext, $iv);
+	abstract public function encrypt(string $plaintext, string $iv) : string|false;
 
-	abstract public function decrypt($crypted, $iv);
+	abstract public function decrypt(string $crypted, string $iv) : string|false;
 
-	public function __construct($key) {
+	public function __construct(string $key) {
 		$this->key = $key;
 	}
 
 	/**
 	 * Generate an "initialization vector" (This too needs storing for decryption but we can append it to the encrypted data)
 	 */
-	public function generateRandomIv() {
+	public function generateRandomIv() : string {
 		$iv = openssl_random_pseudo_bytes(16);
 		// $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES_256_CBC')); // NON VA, PERCHE' ?
 		// https://stackoverflow.com/questions/34871579/how-to-encrypt-plaintext-with-aes-256-cbc-in-php-using-openssl
 		return $iv;
 	}
 
-	public function getIvsFromSignature($sig) {
-		$str = null;
+	public function getIvsFromSignature(string $sig) : string{
+		$str = '';
 		if ($sig !== null) {
 			$array = explode($this->sig_delimiter, $sig);
 			if (isset($array[1])) {
@@ -37,8 +37,8 @@ abstract class AesBase {
 		return $str;
 	}
 
-	public function getCryptedDataFromSignature($sig) {
-		$str = null;
+	public function getCryptedDataFromSignature(string $sig) : string{
+		$str = '';
 		if ($sig !== null) {
 			$array = explode($this->sig_delimiter, $sig);
 			if (isset($array[0])) {
--- src/Iubar/Crypt/AesCbcPkcs5Padding.php
+++ src/Iubar/Crypt/AesCbcPkcs5Padding.php
@@ -6,19 +6,19 @@ namespace Iubar\Crypt;
  *  Implementa l'algoritmo AES/CBC/PKCS5Padding
  */
 class AesCbcPkcs5Padding extends AesBase implements AesInterface {
-	protected $method = 'AES-128-CBC';
-	protected $option = OPENSSL_CIPHER_AES_128_CBC; // oppure OPENSSL_RAW_DATA
+    protected string $method = 'AES-128-CBC';
+	protected int $option = OPENSSL_CIPHER_AES_128_CBC; // oppure OPENSSL_RAW_DATA
 
-	public function __construct($key) {
+	public function __construct(string $key) {
 		parent::__construct($key);
 	}
 
-	public function encrypt($plaintext, $iv) {
+	public function encrypt(string $plaintext, string $iv) : string|false {
 		$enc = openssl_encrypt($plaintext, $this->method, $this->key, $this->option, $iv);
 		return base64_encode($enc);
 	}
 
-	public function decrypt($encrypted, $iv) {
+	public function decrypt(string $encrypted, string $iv) : string|false {
 		$encrypted = base64_decode($encrypted);
 		$dec = openssl_decrypt($encrypted, $this->method, $this->key, $this->option, $iv);
 		return $dec;
